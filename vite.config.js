efc97eb342baf49b83ea3c30862661aa7c8d5241
tuxhar <43926459+tuxhar@users.noreply.github.com>
2025-07-24 20:11:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    checker({
+      typescript: false,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{js,jsx}"'
+      }
+    })
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
